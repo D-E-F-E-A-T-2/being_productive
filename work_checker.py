020034db582f040563f9d919a57e391ee0b30c69
Kush Choudhary <kushchoudhary8@gmail.com>
2019-02-20 09:46:31 +0530

    Updates 20-02-2K19

--- work_checker.py
+++ work_checker.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
 from time import *
 
 print('Use \'ctrl+c\' to Exit\n')
@@ -8,18 +9,32 @@ min = 0
 print('Work Checker is running properly!\n')
 
 i = input("Wanna change \'cycle-time\'? (Type \'yes\' or \'YES\' or 1): ")
-if i in ('yes', 'YES', 1):
-  x = int(input('\nSet \'cycle-time\' to ? minutes '))
+if i in ('yes', 'YES', '1'):
+  try:
+    x = int(input('\nSet \'cycle-time\' to ? minutes '))
+    assert x>0
+  except:
+    print("Invalid time!")
+    print('Setting \'cycle-time\' to defualt(5) minutes in next 5 seconds.\n')
+    for _ in range(1,6):
+      print(_)
+      sleep(1)
+    x = 5
 else:
   print('Setting \'cycle-time\' to defualt(5) minutes in next 5 seconds.\n')
-  for _ in range(1,6):
-    print(_)
+  for _ in range(1,5):
+    print(str(_)+'..', end='')
     sleep(1)
+  print(5)
   x = 5
+
 
 while True:
 
   if min ==0:
+    print('')
+    print(ctime())
+    print('')
     print('Cycle is running properly!')
     print('')
     print('COFFE & CODE!!')
